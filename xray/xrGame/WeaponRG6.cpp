a221dd94440c56e0581cd01efc9839510f9dbb6a
OneMorePseudoCoder <73041211+OneMorePseudoCoder@users.noreply.github.com>
2026-04-03 21:22:59 +0300

    Фикс разрядки РГ-6
    Отсюда:
    https://github.com/Shtrecker/xray-1.5.10-2015-/commit/b776eb7a882a56f0f2837918f60b70e81401d6dd

--- xray/xrGame/WeaponRG6.cpp
+++ xray/xrGame/WeaponRG6.cpp
@@ -7,18 +7,17 @@
 #include "actor.h"
 
 #ifdef DEBUG
-#	include "phdebug.h"
+#include "phdebug.h"
 #endif
 
-
 CWeaponRG6::~CWeaponRG6()
-{
-}
+{}
 
-BOOL	CWeaponRG6::net_Spawn				(CSE_Abstract* DC)
+BOOL CWeaponRG6::net_Spawn(CSE_Abstract* DC)
 {
 	BOOL l_res = inheritedSG::net_Spawn(DC);
-	if (!l_res) return l_res;
+	if (!l_res)
+		return l_res;
 
 	if (iAmmoElapsed && !getCurrentRocket())
 	{
@@ -34,11 +33,8 @@ BOOL	CWeaponRG6::net_Spawn				(CSE_Abstract* DC)
 				inheritedRL::SpawnRocket(*fake_grenade_name, this);
 			}
 		}
-//			inheritedRL::SpawnRocket(*fake_grenade_name, this);
 	}
-	
 
-	
 	return l_res;
 };
 
@@ -47,37 +43,37 @@ void CWeaponRG6::Load(LPCSTR section)
 	inheritedRL::Load(section);
 	inheritedSG::Load(section);
 }
+
 #include "inventory.h"
 #include "inventoryOwner.h"
-void CWeaponRG6::FireStart ()
+void CWeaponRG6::FireStart()
 {
-
-	if(GetState() == eIdle	&& getRocketCount() ) 
+	if (GetState() == eIdle	&& getRocketCount() && iAmmoElapsed)
 	{
-		inheritedSG::FireStart ();
+		inheritedSG::FireStart();
 	
 		Fvector p1, d; 
 		p1.set(get_LastFP()); 
 		d.set(get_LastFD());
 
 		CEntity* E = smart_cast<CEntity*>(H_Parent());
-		if (E){
-			CInventoryOwner* io		= smart_cast<CInventoryOwner*>(H_Parent());
-			if(NULL == io->inventory().ActiveItem())
+		if (E)
+		{
+			CInventoryOwner* io = smart_cast<CInventoryOwner*>(H_Parent());
+			if (NULL == io->inventory().ActiveItem())
 			{
-			Log("current_state", GetState() );
-			Log("next_state", GetNextState());
-			Log("item_sect", cNameSect().c_str());
-			Log("H_Parent", H_Parent()->cNameSect().c_str());
+				Log("current_state", GetState());
+				Log("next_state", GetNextState());
+				Log("item_sect", cNameSect().c_str());
+				Log("H_Parent", H_Parent()->cNameSect().c_str());
 			}
-			E->g_fireParams (this, p1,d);
+			E->g_fireParams(this, p1, d);
 		}
 
 		Fmatrix launch_matrix;
 		launch_matrix.identity();
 		launch_matrix.k.set(d);
-		Fvector::generate_orthonormal_basis(launch_matrix.k,
-											launch_matrix.j, launch_matrix.i);
+		Fvector::generate_orthonormal_basis(launch_matrix.k, launch_matrix.j, launch_matrix.i);
 		launch_matrix.c.set(p1);
 
 		if (IsZoomed() && smart_cast<CActor*>(H_Parent()))
@@ -93,24 +89,12 @@ void CWeaponRG6::FireStart ()
 
 			if (HasPick)
 			{
-				//			collide::rq_result& RQ = HUD().GetCurrentRayQuery();
-				Fvector Transference;
-				//Transference.add(p1, Fvector().mul(d, RQ.range));				
+				Fvector Transference;			
 				Transference.mul(d, RQ.range);
 				Fvector res[2];
-/*#ifdef		DEBUG
-				DBG_OpenCashedDraw();
-				DBG_DrawLine(p1,Fvector().add(p1,d),D3DCOLOR_XRGB(255,0,0));
-#endif*/
 				u8 canfire0 = TransferenceAndThrowVelToThrowDir(Transference, CRocketLauncher::m_fLaunchSpeed, EffectiveGravity(), res);
-/*#ifdef DEBUG
-				if(canfire0>0)DBG_DrawLine(p1,Fvector().add(p1,res[0]),D3DCOLOR_XRGB(0,255,0));
-				if(canfire0>1)DBG_DrawLine(p1,Fvector().add(p1,res[1]),D3DCOLOR_XRGB(0,0,255));
-				DBG_ClosedCashedDraw(30000);
-#endif*/
 				if (canfire0 != 0)
 				{
-//					Msg ("d[%f,%f,%f] - res [%f,%f,%f]", d.x, d.y, d.z, res[0].x, res[0].y, res[0].z);
 					d = res[0];
 				};
 			}
@@ -136,16 +120,17 @@ void CWeaponRG6::FireStart ()
 	}
 }
 
-u8 CWeaponRG6::AddCartridge		(u8 cnt)
+u8 CWeaponRG6::AddCartridge(u8 cnt)
 {
 	u8 t = inheritedSG::AddCartridge(cnt);
-	u8 k = cnt-t;
+	u8 k = cnt - t;
 	shared_str fake_grenade_name = pSettings->r_string(*m_ammoTypes[m_ammoType], "fake_grenade_name");
-	while(k){
+	while (k)
+	{
 		--k;
 		inheritedRL::SpawnRocket(*fake_grenade_name, this);
 	}
-	return k;
+	return t;
 }
 
 void CWeaponRG6::OnEvent(NET_Packet& P, u16 type) 
@@ -153,17 +138,21 @@ void CWeaponRG6::OnEvent(NET_Packet& P, u16 type)
 	inheritedSG::OnEvent(P,type);
 
 	u16 id;
-	switch (type) {
-		case GE_OWNERSHIP_TAKE : {
+	switch (type) 
+	{
+		case GE_OWNERSHIP_TAKE: 
+		{
 			P.r_u16(id);
 			inheritedRL::AttachRocket(id, this);
-		} break;
-		case GE_OWNERSHIP_REJECT : 
-		case GE_LAUNCH_ROCKET : 
-			{
-			bool bLaunch = (type==GE_LAUNCH_ROCKET);
-			P.r_u16						(id);
-			inheritedRL::DetachRocket	(id, bLaunch);
-		} break;
+		} 
+		break;
+		case GE_OWNERSHIP_REJECT: 
+		case GE_LAUNCH_ROCKET: 
+		{
+			bool bLaunch = (type == GE_LAUNCH_ROCKET);
+			P.r_u16(id);
+			inheritedRL::DetachRocket(id, bLaunch);
+		} 
+		break;
 	}
 }
